2764f94fd8c79e049df7ecf262f518a93e911fc5
selim13 <selim013@gmail.com>
2017-11-10 10:33:23 +0300

    Remove unused files

--- src/Container.js
+++ src/Container.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-import { bp, gutter } from "./vars";
+import { bp } from "./vars";
 
 export default styled.div`
   max-width: ${bp.lg}px;
   margin: 0 auto;
-  padding: 0 ${gutter / 2}px;
+  padding: 0 8px;
 `;
--- src/vars.js
+++ src/vars.js
@@ -4,5 +4,3 @@ export const bp = {
   lg: 992,
   xl: 1200
 };
-
-export const gutter = 16;
