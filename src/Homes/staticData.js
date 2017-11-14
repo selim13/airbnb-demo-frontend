db8be1469bd0f60c88795c3904678a0e89a2c1c6
selim13 <selim013@gmail.com>
2017-11-14 19:55:26 +0300

    Update Homes static test data

--- src/Homes/staticData.js
+++ src/Homes/staticData.js
@@ -1,6 +1,9 @@
 import img1 from "./1.png";
 import img2 from "./2.png";
 import img3 from "./3.png";
+import img4 from "./4.png";
+import img5 from "./5.png";
+import img6 from "./6.png";
 
 export default [
   {
@@ -12,7 +15,6 @@ export default [
     bedsNumber: 9,
     reviews: { rating: 4, number: 97, status: "Superhost" }
   },
-
   {
     id: 2,
     name: "Your private 3 bedr. riad and exclusive rental",
@@ -22,14 +24,40 @@ export default [
     bedsNumber: 9,
     reviews: { rating: 5, number: 161, status: "Superhost" }
   },
-
   {
     id: 3,
     name: "Dreamy Tropical Tree House",
     image: img3,
     price: 200,
-    roomType: "Entire house",
+    roomType: "Entire treehouse",
     bedsNumber: 9,
     reviews: { rating: 5, number: 364, status: "Superhost" }
+  },
+  {
+    id: 4,
+    name: "Best location old town luxury loft",
+    image: img4,
+    price: 110,
+    roomType: "Entire apartment",
+    bedsNumber: 1,
+    reviews: { rating: 5, number: 369, status: "Superhost" }
+  },
+  {
+    id: 5,
+    name: "Lussuoso. Vista incantevole.",
+    image: img5,
+    price: 83,
+    roomType: "Entire apartment",
+    bedsNumber: 6,
+    reviews: { rating: 5, number: 105, status: "Superhost" }
+  },
+  {
+    id: 6,
+    name: "In the historical center of Lecce",
+    image: img6,
+    price: 72,
+    roomType: "Entire house",
+    bedsNumber: 3,
+    reviews: { rating: 5, number: 221, status: "Superhost" }
   }
 ];
