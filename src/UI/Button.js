a9d6f57da467959cf4846485f3d6de85368c3d0c
selim13 <selim013@gmail.com>
2017-11-17 09:42:58 +0300

    Fix Filter buttons size and color

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export default styled.button`
   display: inline-block;
-  padding: 6.5px 0.75rem;
-  min-width: 88px;
+  padding: 0.5rem 1rem;
 
   border: 1px solid rgba(72, 72, 72, 0.2);
   border-radius: 4px;
 
+  color: #383838;
   font-family: inherit;
   font-size: 14px;
   font-weight: normal;
