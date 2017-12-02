2977fa0bda05052478638f4009268b44f157ba69
selim13 <selim013@gmail.com>
2017-12-02 23:47:30 +0300

    Scroll page to the top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import ScrollToTop from "./UI/ScrollToTop";
 import Landing from "./Landing";
 import Homes from "./Homes";
 
@@ -8,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <ScrollToTop>
           <Route exact path="/" component={Landing} />
           <Route path="/homes" component={Homes} />
-        </div>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }
